6ee1b7ff0a3cc5cce94bc5cdd034115196b907e3
Jerry Krinock <jerry@sheepsystems.com>
2014-03-26 13:33:49 -0700

    BookMacster 1.21.1 Release Candidate Added helpAssumeServerUnchangedDuringExports Added workaround for Apple Bug ID 16424156

--- NSString+SSYExtraUtils.m
+++ NSString+SSYExtraUtils.m
@@ -2,6 +2,133 @@
 #import "NSScanner+GeeWhiz.h"
 #import "NSBundle+MainApp.h"
 
+/*
+ @brief    A patched version of CFXMLCreateStringByUnescapingEntities(), which
+ fixes Apple Bug ID 16424156
+ @details  See http://lists.apple.com/archives/cocoa-dev/2014/Mar/msg00343.html
+ */
+CFStringRef PatchedCFXMLCreateStringByUnescapingEntities(CFAllocatorRef allocator, CFStringRef string, CFDictionaryRef entitiesDictionary) {
+    if (string == NULL) {
+        return NULL ;
+    }
+    
+    CFStringInlineBuffer inlineBuf; /* use this for fast traversal of the string in question */
+    CFStringRef sub;
+    CFIndex lastChunkStart, length = CFStringGetLength(string);
+    CFIndex i, entityStart;
+    UniChar uc;
+    UInt32 entity;
+    int base;
+    CFMutableDictionaryRef fullReplDict = entitiesDictionary ? CFDictionaryCreateMutableCopy(allocator, 0, entitiesDictionary) : CFDictionaryCreateMutable(allocator, 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+    
+    CFDictionaryAddValue(fullReplDict, (const void *)CFSTR("amp"), (const void *)CFSTR("&"));
+    CFDictionaryAddValue(fullReplDict, (const void *)CFSTR("quot"), (const void *)CFSTR("\""));
+    CFDictionaryAddValue(fullReplDict, (const void *)CFSTR("lt"), (const void *)CFSTR("<"));
+    CFDictionaryAddValue(fullReplDict, (const void *)CFSTR("gt"), (const void *)CFSTR(">"));
+    CFDictionaryAddValue(fullReplDict, (const void *)CFSTR("apos"), (const void *)CFSTR("'"));
+    
+    CFStringInitInlineBuffer(string, &inlineBuf, CFRangeMake(0, length));
+    // The above range was length-1, but that misses the ';' in case the
+    // subject string ends in a numeric HTML entity.  So I removed the "-1"
+    CFMutableStringRef newString = CFStringCreateMutable(allocator, 0);
+    
+    lastChunkStart = 0;
+    // Scan through the string in its entirety
+    for(i = 0; i < length; ) {
+        uc = CFStringGetCharacterFromInlineBuffer(&inlineBuf, i); i++;	// grab the next character and move i.
+        
+        if(uc == '&') {
+            entityStart = i - 1;
+            entity = 0xFFFF;	// set this to a not-Unicode character as sentinel
+            // We may have hit the beginning of an entity. Copy everything from lastChunkStart to this point.
+            if(lastChunkStart < i - 1) {
+                sub = CFStringCreateWithSubstring(allocator, string, CFRangeMake(lastChunkStart, (i - 1) - lastChunkStart));
+                CFStringAppend(newString, sub);
+                CFRelease(sub);
+            }
+            
+            uc = CFStringGetCharacterFromInlineBuffer(&inlineBuf, i); i++;	// grab the next character and move i.
+            // Now we can process the entity reference itself
+            if(uc == '#') {	// If this turns out to be an entity, it is a numeric entity.
+                base = 10;
+                entity = 0;
+                uc = CFStringGetCharacterFromInlineBuffer(&inlineBuf, i); i++;
+                
+                if(uc == 'x') {	// only lowercase x allowed. Translating numeric entity as hexadecimal.
+                    base = 16;
+                    uc = CFStringGetCharacterFromInlineBuffer(&inlineBuf, i); i++;
+                }
+                
+                // process the provided digits 'til we're finished
+                while(true) {
+                    if (uc >= '0' && uc <= '9')
+                        entity = entity * base + (uc-'0');
+                    else if (uc >= 'a' && uc <= 'f' && base == 16)
+                        entity = entity * base + (uc-'a'+10);
+                    else if (uc >= 'A' && uc <= 'F' && base == 16)
+                        entity = entity * base + (uc-'A'+10);
+                    else break;
+                    
+                    if (i < length) {
+                        uc = CFStringGetCharacterFromInlineBuffer(&inlineBuf, i); i++;
+                    }
+                    else
+                        break;
+                }
+            }
+            
+            while(uc != ';' && i < length) {
+                uc = CFStringGetCharacterFromInlineBuffer(&inlineBuf, i); i++;
+            }
+            
+            if (uc == ';') {
+                if(0xFFFF != entity) { // it was numeric, and translated.
+                    // Now, output the result fo the entity
+                    if(entity >= 0x10000) {
+                        UniChar characters[2] = { ((entity - 0x10000) >> 10) + 0xD800, ((entity - 0x10000) & 0x3ff) + 0xDC00 };
+                        CFStringAppendCharacters(newString, characters, 2);
+                    } else {
+                        UniChar character = entity;
+                        CFStringAppendCharacters(newString, &character, 1);
+                    }
+                } else {	// it wasn't numeric.
+                    sub = CFStringCreateWithSubstring(allocator, string, CFRangeMake(entityStart + 1, (i - entityStart - 2))); // This trims off the & and ; from the string, so we can use it against the dictionary itself.
+                    CFStringRef replacementString = (CFStringRef)CFDictionaryGetValue(fullReplDict, sub);
+                    if(replacementString) {
+                        CFStringAppend(newString, replacementString);
+                    } else {
+                        CFRelease(sub); // let the old substring go, since we didn't find it in the dictionary
+                        sub =  CFStringCreateWithSubstring(allocator, string, CFRangeMake(entityStart, (i - entityStart))); // create a new one, including the & and ;
+                        CFStringAppend(newString, sub); // ...and append that.
+                    }
+                    CFRelease(sub); // in either case, release the most-recent "sub"
+                }
+            }
+            else {
+                // Trailing semicolon was missing.  This was not an html entity.
+                // Back out of it.
+                CFStringRef sub1 =  CFStringCreateWithSubstring(allocator, string, CFRangeMake(entityStart, (i - entityStart)));
+                CFStringAppend(newString, sub1) ;
+                if (sub1 != NULL) {
+                    CFRelease(sub1) ;
+                }
+            }
+            
+            // move the lastChunkStart to the beginning of the next chunk.
+            lastChunkStart = i;
+        }
+    }
+    if(lastChunkStart < length) { // we've come out of the loop, let's get the rest of the string and tack it on.
+        sub = CFStringCreateWithSubstring(allocator, string, CFRangeMake(lastChunkStart, i - lastChunkStart));
+        CFStringAppend(newString, sub);
+        CFRelease(sub);
+    }
+    
+    CFRelease(fullReplDict);
+    
+    return newString ;
+}
+
 @implementation NSString (SSYExtraUtils)
 
 + (NSString*)stringWithPascalString:(ConstStr255Param)pascalString
@@ -486,12 +613,8 @@ NSString* const aNewline = @"\n" ;
 }
 
 - (NSString *) stringByDecodingXMLEntities {
-    NSString *unescapedString = (NSString *)CFXMLCreateStringByUnescapingEntities(NULL, (CFStringRef)self, NULL);
-    // Apple documentation for the above function is very confusing regarding what the
-	// last argument does.  I tried several examples.  It seems to always replace
-	// something of the form &#dd; where dd is 1-3 decimal digits, regardless
-	// of what the last argument is.  I have never seen any effect from the last argument.
-	return [unescapedString autorelease];
+    NSString *unescapedString = (NSString *)PatchedCFXMLCreateStringByUnescapingEntities(NULL, (CFStringRef)self, NULL) ;
+	return [unescapedString autorelease] ;
 }
 
 - (NSString*)reverseAsciiChars {
