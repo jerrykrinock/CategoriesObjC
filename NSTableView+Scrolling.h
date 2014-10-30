7a51515432a982c62565c8e7cb5453f895e00923
Jerry Krinock <jerry@sheepsystems.com>
2014-10-29 22:23:36 -0400

    BkmkMgrs 1.22.23 Release Candidate 4
    •  Removed some debug code in ContentDataSource
    •  Fixed NSTableView+Scrolling to work as expected in all rows.  I still don't understand this, but at least it's self-consistent now and also works as expected in a test app.

--- NSTableView+Scrolling.h
+++ NSTableView+Scrolling.h
@@ -1,3 +1,5 @@
+#import <Cocoa/Cocoa.h>
+
 @interface NSTableView (Scrolling)
 
 - (void)scrollRowToTop:(NSInteger)row ;
--- NSTableView+Scrolling.m
+++ NSTableView+Scrolling.m
@@ -5,14 +5,10 @@
 - (void)scrollRowToTop:(NSInteger)row {
 	if ((row != NSNotFound) && (row >=0)) {
 		CGFloat rowPitch = [self rowHeight] + [self intercellSpacing].height ;
-#warning Why do I need to fudge when scrolling to row 0?
-        NSInteger fudgeRow = 0 ;
-        CGFloat fudgePoints = 0 ;
-        if (row == 0) {
-            fudgeRow = -1 ;
-            fudgePoints = -1.0 ;
-        }
-		CGFloat y = (row + fudgeRow) * rowPitch + fudgePoints ;
+        /*  I don't understand why we need to subtract 1 from row in the next
+         statement.  But if we don't, passing row=0 will make visible the
+         second row instead of the first row, and so on. */
+        CGFloat y = (row - 1) * rowPitch -1.0 ;
         [self scrollPoint:NSMakePoint(0, y)] ;
 	}
 }
