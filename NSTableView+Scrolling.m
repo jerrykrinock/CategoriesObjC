085684db441f4849a340fd5799361760c50dc6bf
Jerry Krinock <jerry@sheepsystems.com>
2014-11-06 21:46:56 -0500

    BkmkMgrs 1.22.23 Release Candidate 13
    Removed SSYDBL log lines.

--- NSTableView+Scrolling.m
+++ NSTableView+Scrolling.m
@@ -11,26 +11,19 @@
     CGFloat y = visibleRect.origin.y ;
     CGFloat H = headerRect.size.height ;
     CGFloat C = clipRect.size.height ;
-    /*SSYDBL*/ NSLog(@"uncorr:   %@", NSStringFromRect(visibleRect)) ;
-    /*SSYDBL*/ NSLog(@"v=%0.1f  y=%0.1f  H=%0.1f  C=%0.1f", v, y, H, C) ;
 
     visibleRect.size.height = C - H ;
 
     if (y < 0.01) {
         // Case 1.
-        /*SSYDBL*/ NSLog(@"Case 1") ;
         visibleRect.origin.y = v - visibleRect.size.height ;
     }
     else {
-        /*SSYDBL*/ NSLog(@"Case 2") ;
         // Case 2.
         visibleRect.origin.y += H ;
     }
     
-    /*SSYDBL*/ NSLog(@"corr:     %@", NSStringFromRect(visibleRect)) ;
-    /*SSYDBL*/ NSLog(@"------------------------") ;
 
-//    /*SSYDBL*/ NSLog(@"visiRows: %@", NSStringFromRect(visibleRowsRect)) ;
 
     
     return visibleRect ;
@@ -45,13 +38,11 @@
         y = -[[self headerView] frame].size.height ;
         y += (row) * rowPitch ;
 	}
-    /*SSYDBL*/ CGFloat yB4 = y ;
 
     y += extraPoints ;
     
     [self scrollPoint:NSMakePoint(0, y)] ;
     // Above is o low-level method that scrolls Content, 1 of 2
-    /*SSYDBL*/ NSLog(@"Scrolled to y = %f = %f + %f so that row %ld is at top", y, yB4, extraPoints, (long)row) ;
 }
 
 @end
