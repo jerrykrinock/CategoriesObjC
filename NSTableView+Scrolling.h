5b92d918c80a534672660eef85898027a260c446
Jerry Krinock <jerry@sheepsystems.com>
2014-11-08 14:24:39 -0500

    Now features "smooth" scroll restoration, due to extraPoints parameter

--- NSTableView+Scrolling.h
+++ NSTableView+Scrolling.h
@@ -7,4 +7,6 @@
 - (void)scrollRowToTop:(NSInteger)row
        plusExtraPoints:(CGFloat)extraPoints ;
 
+- (void)scrollRowPoint:(NSPoint)point ;
+
 @end
--- NSTableView+Scrolling.m
+++ NSTableView+Scrolling.m
@@ -29,7 +29,6 @@
     return visibleRect ;
 }
 
-
 - (void)scrollRowToTop:(NSInteger)row
        plusExtraPoints:(CGFloat)extraPoints {
     CGFloat y = 0 ;
@@ -37,12 +36,17 @@
 		CGFloat rowPitch = [self rowHeight] + [self intercellSpacing].height ;
         y = -[[self headerView] frame].size.height ;
         y += (row) * rowPitch ;
+        
+        y += extraPoints ;
+        
+        [self scrollRowPoint:NSMakePoint(0, y)] ;
+        // Above is o low-level method that scrolls Content, 1 of 2
 	}
+}
 
-    y += extraPoints ;
-    
-    [self scrollPoint:NSMakePoint(0, y)] ;
-    // Above is o low-level method that scrolls Content, 1 of 2
+- (void)scrollRowPoint:(NSPoint)point {
+    point.y -= [[self headerView] frame].size.height ;
+    [self scrollPoint:point] ;
 }
 
 @end
