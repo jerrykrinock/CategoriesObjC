e0e5a3acc8364ab31d91c27cc898e39451d3ea1d
Jerry Krinock <jerry@sheepsystems.com>
2016-09-29 17:38:54 -0700

    •  Now checks for nil parameter in -[NSArray(DespoofStrings) replaceOccurrencesOfString:withString:], avoiding crash/exception

--- NSArray+DespoofStrings.h
+++ NSArray+DespoofStrings.h
@@ -4,14 +4,12 @@
 @interface NSArray (DespoofStrings)
 
 /*!
- @brief    For an array of strings, if any string contains a given string, replaces all
- occurrences of a given string in any of them with another given string and returns a
- new array, otherwise returns nil.
+ @brief    For an array of strings, if any string contains a given string,
+ replaces all occurrences of a given 'target' string in any of them with another
+ given 'replacemeent' string and returns a new array, otherwise returns nil.
 
- @details  The receiver must be a set of NSString objects.  If one or more spoofs are
- found in any member of the receiver, returns a new array, a copy of the receiver, but with
- the spoofs replaced by the underscore ("_") character.  If no spoofs are found
- in any string in the receiver, returns nil.
+ @details  The receiver must be a set of NSString objects.  If either parameter
+ is nil, this method returns nil.
  @result   Either a copy of the receiver with the spoofs replaced, or nil.
  */
 - (NSArray*)replaceOccurrencesOfString:(NSString*)target
--- NSArray+DespoofStrings.m
+++ NSArray+DespoofStrings.m
@@ -4,40 +4,43 @@
 
 - (NSArray*)replaceOccurrencesOfString:(NSString*)target
 							withString:(NSString*)replacement {
-	//  This is written to be efficient, assuming patches will be rare.
+    NSArray* despoofedArray = nil ;
+
+    if (target && replacement) {
+        //  This is written to be efficient, assuming patches will be rare.
 	
-	// If any spoofs are found, patches will be a dictionary with
-	//   key   = index of string containing one or more target strings
-	//   value = string with spoofs replaced by underscore
-	NSMutableDictionary* patches = nil ;
-	NSInteger index = 0 ;
-	for (NSString* string in self) {
-		if (([string containsString:target])) {
-			NSMutableString* newTag = [string mutableCopy] ;
-			[newTag replaceOccurrencesOfString:target
-									withString:replacement] ;
-			if (!patches) {
-				patches = [[NSMutableDictionary alloc] init] ;
-			}
-			[patches setObject:newTag forKey:[NSNumber numberWithInteger:index]] ;
-			[newTag release] ;
-		}
-		index++ ;
-	}
-	
-	NSArray* despoofedArray = nil ;
-	if (patches) {
-		NSMutableArray* newArray = [self mutableCopy] ;
-		for (NSNumber* oIndex in patches) {
-			[newArray replaceObjectAtIndex:[oIndex integerValue]
-							   withObject:[patches objectForKey:oIndex]] ;
-		}
-		[patches release] ;
-		
-		despoofedArray = [NSArray arrayWithArray:newArray] ;
-		[newArray release] ;
-	}
-	
+        // If any spoofs are found, patches will be a dictionary with
+        //   key   = index of string containing one or more target strings
+        //   value = string with spoofs replaced by underscore
+        NSMutableDictionary* patches = nil ;
+        NSInteger index = 0 ;
+        for (NSString* string in self) {
+            if (([string containsString:target])) {
+                NSMutableString* newTag = [string mutableCopy] ;
+                [newTag replaceOccurrencesOfString:target
+                                        withString:replacement] ;
+                if (!patches) {
+                    patches = [[NSMutableDictionary alloc] init] ;
+                }
+                [patches setObject:newTag forKey:[NSNumber numberWithInteger:index]] ;
+                [newTag release] ;
+            }
+            index++ ;
+        }
+        
+        if (patches) {
+            NSMutableArray* newArray = [self mutableCopy] ;
+            for (NSNumber* oIndex in patches) {
+                [newArray replaceObjectAtIndex:[oIndex integerValue]
+                                    withObject:[patches objectForKey:oIndex]] ;
+            }
+            [patches release] ;
+            
+            despoofedArray = [NSArray arrayWithArray:newArray] ;
+            [newArray release] ;
+        }
+    }
+    
 	return despoofedArray ;
 }
 
