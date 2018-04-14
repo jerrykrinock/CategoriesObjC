7ab55b2e3b05cce212ca7cdafb3fa13229fc63f7
Jerry Krinock <jerry@sheepsystems.com>
2018-04-14 08:55:26 -0700

    Added new NSManagedObject(SSYCopying) category which I need to support the new "pasteboard" managed object context in my ClassesObjC repo.

---
+++ NSManagedObject+SSYCopying.h
@@ -0,0 +1,60 @@
+#import <CoreData/CoreData.h>
+
+@interface NSManagedObject (SSYCopying)
+
+/*!
+ @brief    Returns a "copy" of the receiver, inserted into a given managed
+ object context, having all of the same attributes as the receiver, but ignoring
+ all of the receivers relationships
+ */
+- (NSManagedObject*)shallowCopyInManagedObjectContext:(NSManagedObjectContext*)targetMoc ;
+
+/*!
+ @brief    Returns a "copy" of the receiver, inserted into a given managed
+ object context, having all of the same attributes as the receiver, copying
+ relationships by inserting additional objects as required to fulfill all
+ relationships, except specified relationship(s)
+ 
+ @param    doNotEnterRelationships  Those of the receiver's
+ relationships which will be skipped; in other words, the copy will be
+ "shallow" with respect to these relationships.  This is to avoid infinite loops
+ wherein related objects would copy one another infinitely.  Typically, you
+ pass a set containing one object, the name of the relationship to the
+ receiver's "parent" in your object graph.  This parameter may be nil.
+ 
+ Sending this message to an object which has a loop in their object graph would,
+ I suspect, result in an infinite loop.
+ */
+- (NSManagedObject*)deepCopyInManagedObjectContext:(NSManagedObjectContext*)targetMoc
+                           doNotEnterRelationships:(NSSet*)relationships ;
+
+/*!
+ @brief    Returns the result of
+ -deepCopyInManagedObjectContext:doNotEnterRelationships: obtained by passing
+ a set of relationships specified by a set of given relationship names.
+ */
+- (NSManagedObject*)deepCopyInManagedObjectContext:(NSManagedObjectContext*)targetMoc
+                       doNotEnterRelationshipNames:(NSSet*)relationshipNames ;
+
+/*!
+ @brief    Invokes deepCopyInManagedObjectContext:doNotEnterRelationshipNames:
+ with a single *do not enter* relationship name
+ */
+- (NSManagedObject*)deepCopyInManagedObjectContext:(NSManagedObjectContext*)targetMoc
+                        doNotEnterRelationshipName:(NSString*)relationshipName ;
+
+/*!
+ @brief    Invokes deepCopyInManagedObjectContext:doNotEnterRelationshipNames:
+ with zero *do not enter* relationship names
+ @details  Be careful with this one that you won't create an infinite loop.
+ */
+- (NSManagedObject*)deepCopyInManagedObjectContext:(NSManagedObjectContext*)targetMoc ;
+
+/*!
+ @brief    Gives all attributes
+ @details  This method is for debugging.
+ */
+- (NSString*)deepDescriptionIgnoringRelationshipWithName:(NSString*)ignoredName ;
+
+
+@end
---
+++ NSManagedObject+SSYCopying.m
@@ -0,0 +1,210 @@
+#import "NSManagedObject+SSYCopying.h"
+#import "NSObject+RecklessPerformSelector.h"
+
+
+@implementation NSManagedObject (SSYCopying)
+
+- (NSManagedObject*)shallowCopyInManagedObjectContext:(NSManagedObjectContext*)targetMoc {
+    NSEntityDescription* entity = self.entity ;
+    NSManagedObject* newObject = [NSEntityDescription insertNewObjectForEntityForName:entity.name
+                                                               inManagedObjectContext:targetMoc] ;
+    /* Another up-vote for Marcus Zarra:
+     http://stackoverflow.com/questions/2998613/how-do-i-copy-or-move-an-nsmanagedobject-from-one-context-to-another
+     */
+    NSDictionary* attributeDic = [self dictionaryWithValuesForKeys:[[entity attributesByName] allKeys]] ;
+    [newObject setValuesForKeysWithDictionary:attributeDic] ;
+    
+    return newObject ;
+}
+
+- (NSManagedObject*)deepCopyInManagedObjectContext:(NSManagedObjectContext*)targetMoc
+                           doNotEnterRelationships:(NSSet <NSRelationshipDescription*> *)doNoEnterRelationships {
+    NSEntityDescription* entity = self.entity ;
+    NSManagedObject* __block newObject = nil ;
+    [targetMoc performBlockAndWait:^(void) {
+        newObject = [NSEntityDescription insertNewObjectForEntityForName:entity.name
+                                                  inManagedObjectContext:targetMoc] ;
+        NSDictionary* __block attributeDic = nil ;
+        [[self managedObjectContext] performBlockAndWait:^(void) {
+            attributeDic = [self dictionaryWithValuesForKeys:[[entity attributesByName] allKeys]] ;
+        }] ;
+        [newObject setValuesForKeysWithDictionary:attributeDic] ;
+    }] ;
+
+    NSSet <NSString*> * doNotEnterKeys = [doNoEnterRelationships valueForKey:@"name"]  ;
+    NSDictionary* relationships = [entity relationshipsByName] ;
+    for (NSString* key in relationships) {
+        if (![doNotEnterKeys member:key]) {
+            NSRelationshipDescription* relationship = [relationships objectForKey:key] ;
+            NSRelationshipDescription* inverseRelationship = [relationship inverseRelationship] ;
+            NSAssert(
+                     (inverseRelationship != nil),
+                     @"No inverse relationship in %@ from %@ in %@",
+                     relationship.destinationEntity.name,
+                     relationship.name,
+                     self.className) ;
+            NSString* inverseKey = inverseRelationship.name ;
+            // Note: Property .toMany requires OS X 10.10 or later
+            if (relationship.toMany) {
+                // To-Many Relationship
+                NSObject <NSFastEnumeration> __block * oldCollection = nil ;
+                [[self managedObjectContext] performBlockAndWait:^(void) {
+                    oldCollection = [self valueForKey:key] ;
+                }] ;
+
+                /* The following assumes that you have used mogenerator
+                 or equivalent to define add<Key>Object: setters for all of
+                 your managed object's to-many relationships. */
+                NSString* addObjectSetterName = [[NSString alloc] initWithFormat:
+                                                 @"add%@Object:",
+                                                 [key capitalizedString]] ;
+                SEL addObjectSelector = NSSelectorFromString(addObjectSetterName) ;
+#if !__has_feature(objc_arc)
+                [addObjectSetterName release];
+#endif
+                [[self managedObjectContext] performBlockAndWait:^(void) {
+                    for (NSManagedObject* oldChild in oldCollection) {
+                        NSManagedObject* newChild = [oldChild deepCopyInManagedObjectContext:targetMoc
+                                                                      doNotEnterRelationships:[NSSet setWithObject:inverseRelationship]] ;
+                        
+                        // Wire up relationship, and its inverse relationship.
+                        [targetMoc performBlockAndWait:^(void) {
+                            [newObject recklessPerformVoidSelector:addObjectSelector
+                                                            object:newChild] ;
+                            [newChild setValue:newObject
+                                        forKey:inverseKey] ;
+                        }] ;
+                    }
+                }] ;
+            }
+            else {
+                // To-One Relationship
+                NSManagedObject* __block oldChild = nil ;
+                [[self managedObjectContext] performBlockAndWait:^(void) {
+                    oldChild = [self valueForKey:key] ;
+                }] ;
+                NSManagedObject* newChild = [oldChild deepCopyInManagedObjectContext:targetMoc
+                                                             doNotEnterRelationships:[NSSet setWithObject:inverseRelationship]] ;
+                [targetMoc performBlockAndWait:^(void) {
+                    // Wire up relationship, and its inverse relationship.
+                    [newObject setValue:newChild
+                                 forKey:key] ;
+                    [newChild setValue:newObject
+                                forKey:inverseKey] ;
+                }] ;
+            }
+        }
+    }
+    
+    return newObject ;
+}
+
+- (NSManagedObject*)deepCopyInManagedObjectContext:(NSManagedObjectContext*)targetMoc
+                       doNotEnterRelationshipNames:(NSSet*)relationshipNames {
+    NSDictionary* relationships = [self.entity relationshipsByName] ;
+    NSMutableSet <NSRelationshipDescription*> * doNotEnterRelationships = [NSMutableSet new] ;
+    for (NSString* name in relationshipNames) {
+        NSRelationshipDescription* relationship = [relationships objectForKey:name] ;
+        if (relationship) {
+            [doNotEnterRelationships addObject:relationship] ;
+        }
+    }
+
+    NSSet* doNotEnterRelationshipsCopy = [doNotEnterRelationships copy];
+    NSManagedObject* answer = [self deepCopyInManagedObjectContext:targetMoc
+                                           doNotEnterRelationships:doNotEnterRelationshipsCopy] ;
+#if !__has_feature(objc_arc)
+    [doNotEnterRelationships release];
+    [doNotEnterRelationshipsCopy release];
+#endif
+
+    return answer;
+}
+
+- (NSManagedObject*)deepCopyInManagedObjectContext:(NSManagedObjectContext*)targetMoc
+                        doNotEnterRelationshipName:(NSString*)relationshipName {
+    return [self deepCopyInManagedObjectContext:targetMoc
+                    doNotEnterRelationshipNames:[NSSet setWithObject:relationshipName]] ;
+}
+
+- (NSManagedObject*)deepCopyInManagedObjectContext:(NSManagedObjectContext*)targetMoc {
+    return [self deepCopyInManagedObjectContext:targetMoc
+                    doNotEnterRelationshipNames:nil] ;
+}
+
+- (void)deepDescriptionIntoString:(NSMutableString*)string
+           fromParentRelationship:(NSRelationshipDescription*)fromParentRelationship {
+    NSEntityDescription* entity = self.entity ;
+    NSDictionary* attributeDic = [self dictionaryWithValuesForKeys:[[entity attributesByName] allKeys]] ;
+    [string appendFormat:
+     @"Attributes for %@:\n%@",
+     entity.name,
+     attributeDic] ;
+    NSRelationshipDescription* fromParentInverseRelationship =[fromParentRelationship inverseRelationship] ;
+    NSString* fromParentInverseKey = fromParentInverseRelationship.name ;
+    NSDictionary* relationships = [entity relationshipsByName] ;
+    for (NSString* key in relationships) {
+        if (![key isEqualToString:fromParentInverseKey]) {
+            NSRelationshipDescription* relationship = [relationships objectForKey:key] ;
+            NSRelationshipDescription* inverseRelationship =[relationship inverseRelationship] ;
+            NSAssert(
+                     (inverseRelationship != nil),
+                     @"No inverse relationship in %@ from %@ in %@",
+                     relationship.destinationEntity.name,
+                     relationship.name,\
+                     self.className) ;
+            // Note: Property .toMany requires OS X 10.10 or later
+            if (relationship.toMany) {
+                // To-Many Relationship
+                NSObject <NSFastEnumeration> * oldCollection = [self valueForKey:key] ;
+                NSInteger i = 0 ;
+                for (NSManagedObject* oldChild in oldCollection) {
+                    [string appendFormat:
+                     @"Digging into %@ index %ld...",
+                     relationship.name,
+                     (long)i] ;
+                    [oldChild deepDescriptionIntoString:string
+                                 fromParentRelationship:relationship] ;
+                    i++ ;
+                }
+            }
+            else {
+                // To-One Relationship
+                NSManagedObject* oldChild = [self valueForKey:key] ;
+                [oldChild deepDescriptionIntoString:string
+                             fromParentRelationship:relationship] ;
+            }
+        }
+    }
+}
+
+- (NSString*)deepDescriptionIgnoringRelationshipWithName:(NSString*)ignoredName {
+    NSMutableString* dd = [[NSMutableString alloc] initWithFormat:
+                           @"***********************\nDeep Description of %@ %p in moc named %@\n",
+                           [self className],
+                           (__bridge void*)self,
+                           self.managedObjectContext.name] ;
+
+    NSEntityDescription* entity = self.entity ;
+    NSRelationshipDescription* fromParentRelationshipToIgnore = nil ;
+    if (ignoredName) {
+        NSDictionary* relationships = [entity relationshipsByName] ;
+        for (NSString* key in relationships) {
+            if ([key isEqualToString:ignoredName]) {
+                NSRelationshipDescription* fromChildRelationship = [relationships objectForKey:key] ;
+                fromParentRelationshipToIgnore = [fromChildRelationship inverseRelationship] ;
+            }
+        }
+    }
+    
+    [self deepDescriptionIntoString:dd
+             fromParentRelationship:fromParentRelationshipToIgnore] ;
+    NSString* answer = [dd copy];
+#if !__has_feature(objc_arc)
+    [dd release];
+    [answer autorelease];
+#endif
+    return answer;
+}
+
+@end
