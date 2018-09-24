ef5735ef3273fd8a2e90cf4303e0e3a72d347e0a
Jerry Krinock <jerry@sheepsystems.com>
2018-09-24 12:04:34 -0700

    Worked around what may be a bug in macOS 10.14, that if the process is a bare executable, [[NSBundle mainBundle] bundlePath] will return the parent of the symbolic link instead of the path to the bundle that the actual executable is in.  I may be wrong but don't have time to investigate further right now.

--- NSBundle+MainApp.m
+++ NSBundle+MainApp.m
@@ -8,8 +8,23 @@ static NSBundle* mainAppBundle = nil ;
 + (NSBundle*)mainAppBundle {
     @synchronized(self) {
         if (!mainAppBundle) {
-            NSBundle* innermostBundle = [NSBundle mainBundle] ;
-            NSString* path = [innermostBundle bundlePath] ;
+            /* See if path is a symbolic link, because if this process
+             was launched by a symbolic link, starting in macOS 10.14,
+             this path will be the symbolic link's path!!
+
+             (Similarly, if this process is a bare executable, and was
+             launched via symbolic link, [[NSBundle mainBundle] bundlePath]
+             will return the parent of the symbolic link, which is useless
+             for our purpose here.)  I have not  tested what
+             [[NSBundle mainBundle] bundlePath] returns if this process is in a
+             bundle but launched via a symbolic link. */
+            NSString* path = [[[NSProcessInfo processInfo] arguments] objectAtIndex:0];
+            NSString* symlinkDestin = [[NSFileManager defaultManager] destinationOfSymbolicLinkAtPath:path
+                                                                                                error:NULL];
+            if (symlinkDestin) {
+                path = symlinkDestin;
+            }
+
             while (path.length > 4) {
                 if ([path hasSuffix:@".app"]) {
 #if !__has_feature(objc_arc)
