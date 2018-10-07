6411faaea4bddfa2e47695d5625feb0e75f95a61
Jerry Krinock <jerry@sheepsystems.com>
2018-10-07 05:07:09 -0700

    Added -[NSProcessInfo(SSYMoreInfo currentMemorySizeError_p:].  Does not work as expected, but I might come back and work on it later if I decide I really need it.

--- NSProcessInfo+SSYMoreInfo.h
+++ NSProcessInfo+SSYMoreInfo.h
@@ -10,5 +10,21 @@
  */
 - (NSDictionary*)geekyProcessInfo ;
 
+/*!
+ @brief
+
+ @details  I'm not sure why this does not work.  It gives me a number which
+ is ~1000 times bigger than the number given by ps -axww -o vsz -o rss, and
+ 128 times bigger than that of Activity Monitor.  Example:
+
+ Activity Monitor "Memory" for this process = 38.5 MB
+ `ps -axww -o vsz -o pid` prints 5039944 for this process
+ This method returns 5,155,213,312 which looks to me like 5.1 GB.
+
+ @param    error_p  Pointer which will, upon return, if an error
+ occurred and said pointer is not NULL, point to an NSError
+ describing said error.
+ */
+- (NSInteger)currentMemorySizeError_p:(NSError**)error_p;
 
 @end
--- NSProcessInfo+SSYMoreInfo.m
+++ NSProcessInfo+SSYMoreInfo.m
@@ -1,6 +1,11 @@
 #import "NSProcessInfo+SSYMoreInfo.h"
 #import "SSYShellTasker.h"
 #import "NSString+SSYExtraUtils.h"
+#import <mach/mach.h>
+#import <mach/message.h>  // for mach_msg_type_number_t
+#import <mach/kern_return.h>  // for kern_return_t
+#import <mach/task_info.h>
+
 
 @implementation NSProcessInfo (SSYMoreInfo)
 
@@ -50,6 +55,40 @@
 			[self environment], @"Environment",
 			parentInfo, @"ParentProcessInfo",
 			nil] ;	
+}
+
+- (NSInteger)currentMemorySizeError_p:(NSError**)error_p {
+    mach_msg_type_number_t count = MACH_TASK_BASIC_INFO_COUNT;
+    mach_task_basic_info_data_t taskInfo;
+    taskInfo.virtual_size = 0;
+    taskInfo.resident_size = 0;
+    taskInfo.resident_size_max = 0;
+    NSInteger size = -1;
+
+    // https://www.gnu.org/software/hurd/gnumach-doc/Task-Information.html
+    kern_return_t kernReturn = task_info(
+                                         mach_task_self(),  // or current_task(), same thing
+                                         MACH_TASK_BASIC_INFO,
+                                         (task_info_t)&taskInfo,
+                                         &count
+                                         );
+    if (kernReturn == KERN_SUCCESS) {
+        mach_vm_size_t vsize = taskInfo.virtual_size ;
+        /* mach_vm_size_t is a uint64_t.  Cast to NSInteger */
+        size = (NSInteger)vsize;
+    } else if (error_p) {
+        NSString* desc = [[NSString alloc] initWithFormat:
+                          @"Could not get memory size due to Mach error %ld",
+                          (long)kernReturn];
+        *error_p = [NSError errorWithDomain:@"SSYMoreInfoErrorDomain"
+                                             code:487538
+                                         userInfo:@{NSLocalizedDescriptionKey:desc}];
+#if !__has_feature(objc_arc)
+        [desc release];
+#endif
+    }
+
+    return size;
 }
 
 
