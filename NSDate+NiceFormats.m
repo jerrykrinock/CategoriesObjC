5be0429082cf84eb9ec46456c269d2e393fdb5a7
Jerry Krinock <jerry@sheepsystems.com>
2019-05-24 23:34:20 -0700

    Updated method for geekDateTimeString to use a date formatter, because I just noticed that the current Apple documenation for -[NSDate description] no longer guarantees a certain date format.  Also, improved efficiency of -geekDateTimeStringMilli by using a static.

--- NSDate+NiceFormats.m
+++ NSDate+NiceFormats.m
@@ -1,6 +1,10 @@
 #import "NSDate+NiceFormats.h"
 #import "NSString+SSYExtraUtils.h"
 
+static NSDateFormatter* static_geekDateFormatter = nil;
+static NSDateFormatter* static_geekMilliDateFormatter = nil;
+
+
 #if (MAC_OS_X_VERSION_MAX_ALLOWED < 1060) 
 
 /*!
@@ -30,60 +34,36 @@ you've checked that you are running under macOS 10.6.
 	return string ;
 }
 
++ (NSDateFormatter*)geekDateFormatter {
+    if (!static_geekDateFormatter) {
+        static_geekDateFormatter = [[NSDateFormatter alloc] init] ;
+        // Colons cannot be used because this will be part of a filename…
+        [static_geekDateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"] ;
+    }
+
+    return static_geekDateFormatter ;
+}
+
++ (NSDateFormatter*)geekMilliDateFormatter {
+    if (!static_geekMilliDateFormatter) {
+        static_geekMilliDateFormatter = [[NSDateFormatter alloc] init] ;
+        // Colons cannot be used because this will be part of a filename…
+        [static_geekMilliDateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss.SSS"] ;
+    }
+
+    return static_geekMilliDateFormatter ;
+}
+
 - (NSString*)geekDateTimeString {
-	// After spending 45 minutes reading NSDateFormatter trying to 
-	// figure out a non-depracated and supported way to get a date
-	// in geek format, I noticed that -[NSDate description] is
-	// *documented* to return a string in this format:
-	//     YYYY-MM-DD HH:MM:SS ±HHMM
-	// Prior to Mac OS 10.7, ±HHMM = the local time zone offset
-	// Starting in Mac OS 10.7, ±HHMM = +0000.
-	// We want the time in the local time zone, so we need to read and adjust if necessary
-	// To find "if necessary", we determine whether or not ±HHMM is equal to the
-	// local time offset.  If it is, no adjustment is necessary.
-	NSString* s = [self description] ;
-	NSInteger tzSign = [[s substringWithRange:NSMakeRange(20,1)] isEqualToString:@"+"] ? +1 : -1 ;
-	NSInteger tzHours = [[s substringWithRange:NSMakeRange(21,2)] integerValue] ;
-	NSInteger tzMinutes = [[s substringWithRange:NSMakeRange(23,2)] integerValue] ;
-	NSInteger tzSeconds = tzSign * (3600*tzHours + 60*tzMinutes) ; 
-	NSInteger localTzSeconds = [[NSTimeZone localTimeZone] secondsFromGMT] ;
-	NSString* localTimeString ;
-	NSTimeInterval adjustment = localTzSeconds - tzSeconds ;
-	// We'd rather not make the adjustment, because -dateByAddingTimeInterval:
-	// is not available in macOS 10.5.  And since we're going to ignore the
-	// seconds anyhow, we allow 30 seconds of slop.
-	if (fabs(adjustment) < 30.0) {
-		// This must be Mac OS 10.5 or 10.6.  No adjustment is necessary.
-		localTimeString = s ;
-	}
-	else {
-		// This must be macOS 10.7 or later.  Must adjust.
-		// Fortunately, starting in Mac OS 10.6 we have -[NSDate dateByAddingTimeInterval]
-		NSDate* localDate = [self dateByAddingTimeInterval:adjustment] ;
-		localTimeString = [localDate description] ;
-	}
-	
-	NSString* answer = [localTimeString substringToIndex:19] ;
-	
-	return answer ;
+	return [[[self class] geekDateFormatter] stringFromDate:self] ;
+}
+
+- (NSString*)geekDateTimeStringMilli {
+    return [[[self class] geekMilliDateFormatter] stringFromDate:self] ;
 }
 
 - (NSString*)hourMinuteSecond {
     return [[self geekDateTimeString] substringFromIndex:11] ;
-}
-
-- (NSString*)geekDateTimeStringMilli {
-	NSString* formatString = @"yyyy-MM-dd HH:mm:ss.SSS" ;
-	
-	NSDateFormatter* formatter ;
-	formatter = [[NSDateFormatter alloc] init] ;
-	[formatter setDateFormat:formatString];
-	
-	NSString* dateString = [formatter stringFromDate:self] ;	
-
-	[formatter release] ;
-	
-	return dateString ;
 }
 
 - (NSString*)compactDateTimeString {
