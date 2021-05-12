66772ff17564b8b5f0d7833e0d3d510748038ea7
Jerry Krinock <jerry@sheepsystems.com>
2021-05-12 12:52:23 -0700

    NSUserDefaults(MoreTypes) methods for accessing colors now use secure coding (+[NSKeyedArchiver archivedDataWithRootObject:requiringSecureCoding:error:] etc.) instead of [NSArchiver archivedDataWithRootObject:], and a new method, -[NSUserDefaults upgradeDeprecatedArchiveDataForOldKey:newKey:], has been added to upgrade users' User Defaults.

--- NSUserDefaults+MoreTypes.h
+++ NSUserDefaults+MoreTypes.h
@@ -1,17 +1,27 @@
 #import <Cocoa/Cocoa.h>
 
 
-/*!
- @brief    
-
- @details  The NSColor methods were copied from
- http://developer.apple.com/documentation/Cocoa/Conceptual/DrawColor/Tasks/StoringNSColorInDefaults.html
-*/
 @interface NSUserDefaults (MoreTypes)
 
 - (void)setColor:(NSColor*)aColor
 		  forKey:(NSString*)aKey ;
 
 - (NSColor*)colorForKey:(NSString*)aKey ;
+
+/*!
+ @brief    Copies a color in User Defaults which was produced with the
+ deprecated (in macOS 10.13) method +[NSArchiver archivedDataWithRootObject:],
+ such as those produced by previous versions of this method, and writes it to
+ a new key, using a secure coding method
+ +[NSKeyedArchiver archivedDataWithRootObject:requiringSecureCoding:error:]
+ which is readable using -colorForKey: in this version.
+ 
+ @details  This method should be used to upgrade users' User Defaults when
+ first shipping a version of your app with this version.  We create a new key
+ instead of overwriting the value in the old key in case some users later
+ downgrade to the previous version, which will be unable to read the new value.
+ */
+- (void)upgradeDeprecatedArchiveDataForOldKey:(NSString*)oldKey
+                                       newKey:(NSString*)newKey;
 
 @end
--- NSUserDefaults+MoreTypes.m
+++ NSUserDefaults+MoreTypes.m
@@ -2,19 +2,56 @@
 
 @implementation NSUserDefaults (MoreTypes)
 
-- (void)setColor:(NSColor *)aColor forKey:(NSString *)aKey {
-    NSData* theData = [NSArchiver archivedDataWithRootObject:aColor] ;
-    [self setObject:theData forKey:aKey] ;
+- (void)setColor:(NSColor *)color forKey:(NSString *)key {
+    NSError* error = nil;
+    NSData* data = [NSKeyedArchiver archivedDataWithRootObject:color
+                                         requiringSecureCoding:YES
+                                                         error:&error];
+    if (error) {
+        NSLog(@"Internal error 382-5849 archiving color for key %@ in user defaults.", key);
+    }
+    [self setObject:data forKey:key] ;
+    /*SSYDBL*/ NSLog(@"For key %@, set new color data %@", key, data);
 }
 
-- (NSColor*)colorForKey:(NSString *)aKey {
-    NSColor* theColor = nil ;
-    NSData* theData = [self dataForKey:aKey] ;
-    if (theData != nil) {
-        theColor = (NSColor*)[NSUnarchiver unarchiveObjectWithData:theData] ;
+- (NSColor*)colorForKey:(NSString *)key {
+    NSColor* color = nil ;
+    NSData* data = [self dataForKey:key] ;
+//    /*SSYDBL*/ NSLog(@"Read new data %@", data);
+    if (data != nil) {
+        NSError* error = nil;
+        color = (NSColor*)[NSKeyedUnarchiver unarchivedObjectOfClass:[NSColor class]
+                                                               fromData:data
+                                                                  error:&error];
+//        /*SSYDBL*/ NSLog(@"unarchived new color %@", color);
+        if (error) {
+            NSLog(@"Internal error 382-5850 unarchiving color for key %@ in user defaults.", key);
+        }
     }
 	
-	return theColor ;
+	return color ;
+}
+
+- (void)upgradeDeprecatedArchiveDataForOldKey:(NSString*)oldKey
+                                       newKey:(NSString*)newKey {
+    NSColor* color = nil ;
+    NSData* data = [[NSUserDefaults standardUserDefaults] dataForKey:oldKey] ;
+    if (data) {
+        @try {
+            // Sorry, Apple: Need to get old color with deprecated method
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+            color = (NSColor*)[NSUnarchiver unarchiveObjectWithData:data] ;
+#pragma GCC diagnostic warning "-Wdeprecated-declarations"
+        } @catch (NSException *exception) {
+            NSLog(@"Exception upgrading color in user defaults for key %@, which was expected to be produced by +[NSArchiver archivedDataWithRootObject:]", oldKey);
+        }
+        if (!color || [color isKindOfClass:[NSColor class]]) {
+            [self setColor:color
+                    forKey:newKey];
+        } else {
+            NSLog(@"Error upgrading color in user defaults for key %@.  Expected NSColor, found %@", oldKey, [color className]);
+        }
+    }
 }
 
 @end
