e398d61784bb3da0686ac0a7df205240ca1b387b
Jerry Krinock <jerry@sheepsystems.com>
2022-09-24 20:44:01 -0700

    Added missing return type to declarations of -[NSObject longDescription] and -[NSObject shortDescription].  We're trying to interoperate with Swift code now :)

--- NSObject+MoreDescriptions.h
+++ NSObject+MoreDescriptions.h
@@ -12,14 +12,14 @@
  description.&nbsp;  Default implementation returns 
  the normal -description.
 */
-- longDescription ;
+- (NSString*)longDescription ;
 
 /*!
  @brief    Subclasses can override to return a short
  description.&nbsp;  Default implementation returns 
  the normal -description.
  */
-- shortDescription ;
+- (NSString*)shortDescription ;
 
 - (NSString*)deepNiceDescription ;
 
